a9f14f6a22e52c763577c7687086e39dffc2cf93
Jess Johnson <jessica.irish.johnson@gmail.com>
2016-05-10 18:24:14 +0000

    passport tweaks

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ var Strategy = require('passport-facebook').Strategy;
 
 var session = require('express-session');
 
+//configure passport
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_KEY,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+
 var app = express();
 require('dotenv').load();
 
@@ -20,6 +36,30 @@ app.get('/', function (req, res) {
 	
 			
 });
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+  
+  app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
